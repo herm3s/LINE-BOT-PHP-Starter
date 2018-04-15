90207b05df949f80ca60ffb90e283aaa4354b75e
herm3s <angeluster@gmail.com>
2018-04-16 03:57:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
 <?php
 
-include_once('LINEBot.php');
-
-$bot = new \LINE-BOT-PHP-Starter\LINEBot(new CurlHTTPClient('r0AE+qmwBHCFStfXuZIaO8HzNHnF2eJ3O4zOQIzzAqJ1nmEV1XJXnmbP++ei7yRQBujrR48im+iuMUD7kGyOagWaDhQwq2TIuOqR2UIW+L6EoSHC2VGxAFnm4syPBpDhWitZM0FSe249Z1EN3xxqMgdB04t89/1O/w1cDnyilFU='), [
-    'channelSecret' => '9f56daa9d7e46e9b82d2081fce3a6dd1'
-]);
 
 
-$user = $bot->getProfile('U127bc358c8b192a31d4be78f8a6d902a');
-
-echo "หวัดดีประชาชน(hmph)(hmph)(hmph)(cony kiss)" . $user;
+echo "หวัดดีประชาชน(hmph)(hmph)(hmph)(cony kiss)" ;
 
 
 
