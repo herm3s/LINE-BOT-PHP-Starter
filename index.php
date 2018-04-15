218cc21405b86376f43b0f5a6452697b828a19ba
herm3s <angeluster@gmail.com>
2018-04-16 03:39:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
 <?php
-echo "หวัดดีประชาชน(hmph)(hmph)(hmph)(cony kiss)";
+
+include_once('LINEBot.php');
+
+use LINE-BOT-PHP-Starter/LINEBot/;
+use LINE-BOT-PHP-Starter/LINEBot/HTTPClient;
+$bot = new \LINE-BOT-PHP-Starter\LINEBot(new CurlHTTPClient('r0AE+qmwBHCFStfXuZIaO8HzNHnF2eJ3O4zOQIzzAqJ1nmEV1XJXnmbP++ei7yRQBujrR48im+iuMUD7kGyOagWaDhQwq2TIuOqR2UIW+L6EoSHC2VGxAFnm4syPBpDhWitZM0FSe249Z1EN3xxqMgdB04t89/1O/w1cDnyilFU='), [
+    'channelSecret' => '9f56daa9d7e46e9b82d2081fce3a6dd1'
+]);
 
 
-$ch = curl_init();
-// Disable SSL verification
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// Will return the response, if false it print the response
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// Set the url
-curl_setopt($ch, CURLOPT_URL,$url);
-// Execute
-$result=curl_exec($ch);
-// Closing
-curl_close($ch);
-// Will dump a beauty json :3
-//var_dump(json_decode($result, true));
-$data = json_decode($result,true);
-echo  "THB :" . $data["THB"];
+$user = $bot->getProfile('U127bc358c8b192a31d4be78f8a6d902a');
+
+echo "หวัดดีประชาชน(hmph)(hmph)(hmph)(cony kiss)" . $user;
+
+
+
