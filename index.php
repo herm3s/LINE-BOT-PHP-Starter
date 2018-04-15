bacf75d3ff479504c20c14b69c6510dcb25fbe43
herm3s <angeluster@gmail.com>
2018-04-16 03:40:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 
 include_once('LINEBot.php');
 
-use LINE-BOT-PHP-Starter/LINEBot/;
-use LINE-BOT-PHP-Starter/LINEBot/HTTPClient;
 $bot = new \LINE-BOT-PHP-Starter\LINEBot(new CurlHTTPClient('r0AE+qmwBHCFStfXuZIaO8HzNHnF2eJ3O4zOQIzzAqJ1nmEV1XJXnmbP++ei7yRQBujrR48im+iuMUD7kGyOagWaDhQwq2TIuOqR2UIW+L6EoSHC2VGxAFnm4syPBpDhWitZM0FSe249Z1EN3xxqMgdB04t89/1O/w1cDnyilFU='), [
     'channelSecret' => '9f56daa9d7e46e9b82d2081fce3a6dd1'
 ]);
