fe9f624f27a98db8050a3bdc75aa4535d02e59aa
herm3s <angeluster@gmail.com>
2018-04-16 00:14:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
 echo "หวัดดีประชาชน(hmph)(hmph)(hmph)(cony kiss)";
+
+
+$ch = curl_init();
+// Disable SSL verification
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+// Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($ch, CURLOPT_URL,$url);
+// Execute
+$result=curl_exec($ch);
+// Closing
+curl_close($ch);
+// Will dump a beauty json :3
+//var_dump(json_decode($result, true));
+$data = json_decode($result,true);
+echo  "THB :" ; echo $data["THB"];
