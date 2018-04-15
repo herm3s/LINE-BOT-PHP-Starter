a8a10a284eb9aaa42c6a4c71a84be594e8a3f13f
herm3s <angeluster@gmail.com>
2018-04-16 00:18:53 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -39,7 +39,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'] && $data["THB"];
+			$text = $event['message']['text'] . $data["THB"];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
